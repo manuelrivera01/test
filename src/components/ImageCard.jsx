72269f89039cf47f5a9a03c106270844a5afdc50
someonebasedkd <someone@someplacebasedkd.com>
2022-02-21 01:12:56 -0600

    first commit

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -8,7 +8,7 @@ const ImageCard = ({ image }) => {
       <img src={image.webformatURL} alt='' className='w-full' />
       <div className='px-6 py-4'>
         <div className='font-bold text-purple-500 text-xl mb-2'>
-          Photo by {image.user}
+          Tomada por {image.user}
         </div>
         <ul>
           <li>
